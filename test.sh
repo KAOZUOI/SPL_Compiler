cd0dfee9c8d3b2b787ada6c190fdb196652c7533
lerrorgk <lerrorgk@163.com>
2023-11-01 14:31:07 +0800

    run

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 # 只跑*.spl文件，不跑*.out文件
 rm -rf test/phase1/test_result && mkdir test/phase1/test_result
-for file in `ls test/phase1/test/ | grep -v .out`
+for file in `ls test/phase1/test/`
 do
     echo "test/phase1/test/$file"
     ./splc test/phase1/test/$file > test/phase1/test_result/${file%.*}.out
